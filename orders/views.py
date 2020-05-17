2d4f450ef3be4c6eec95e9037fdb29dc4dfd073d
marcellagoda <marceli.lagoda@gmail.com>
2020-05-17 18:23:26 +0200

    Add initial orders app files

---
+++ orders/views.py
@@ -0,0 +1,33 @@
+import stripe
+from django.conf import settings
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
+
+
+class OrderPageView(TemplateView):
+    template_name = 'orders/purchase.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['stripe_key'] = settings.STRIPE_TEST_PUBLISHABLE_KEY
+        return context
+
+
+def charge(request):
+    permission = Permission.objects.get(codename='special_status')
+
+    user = request.user
+    user.user_permission.add(permission)
+
+    if request.method == 'POST':
+        charge = stripe.Charge.create(
+            amount=3999,
+            currency='pln',
+            description='Purchase all books',
+            source=request.POST['stripeToken']
+        )
+        template_name = 'orders/charge.html'
+
+        return render(request, template_name)
