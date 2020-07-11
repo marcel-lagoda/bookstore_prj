b73c5af0dc13b02380e77a18b6b31850c05e7ad0
Marcel <marceli.lagoda@gmail.com>
2020-07-11 16:04:01 +0200

    Add searching view

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
@@ -19,3 +20,13 @@ class BookDetailView(LoginRequiredMixin,
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query))
